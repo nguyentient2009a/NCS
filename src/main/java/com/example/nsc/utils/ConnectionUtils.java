b6a58eca992475f751883d6a24a77b75c877081f
nguyentient2009a <tienndth2008051@fpt.edu.vn>
2022-09-12 17:51:26 +0700

    ncs

---
+++ src/main/java/com/example/nsc/utils/ConnectionUtils.java
@@ -0,0 +1,36 @@
+package com.example.nsc.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+    private static Connection connection;
+
+    public synchronized static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/tiezn";
+        String user = "root";
+        String pass = "";
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(url, user, pass);
+            } catch (SQLException | ClassNotFoundException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+            connection = null;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            connection = null;
+        }
+    }
+}
---
+++ src/main/webapp/login.jsp
@@ -0,0 +1,30 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tiezn
+  Date: 9/12/22
+  Time: 2:28 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Login to go home</title>
+    <style>
+
+    </style>
+</head>
+<body>
+<form action="login" method="post">
+
+    Username: <input type="text" name="username" required >
+
+    <br>
+    <br>
+    Password: <input type="password" name="password" required >
+
+    <br>
+    <br>
+    <input type="submit" value="Login">
+</form>
+</body>
+</html>
